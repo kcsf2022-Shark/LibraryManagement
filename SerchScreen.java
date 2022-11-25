551341e8fe9036ea0e9dcb045ef41b9c98e150f5
TaiseiKoga <73147431+KT-kcsf@users.noreply.github.com>
2022-11-25 10:05:23 +0900

    Add files via upload

--- SerchScreen.java
+++ SerchScreen.java
@@ -11,66 +11,58 @@ public class SerchScreen extends JFrame implements ActionListener{
 	private JPanel p1, p2, p3, p4;
 	
 	private Shark s;
-	private boolean pre = true;
 	
 	SerchScreen(){
 		super("検索画面");
 		super.setUndecorated(true);
 		super.setResizable(false);
-		serchPre();
-	}
-	public void serchPre(){
-		if(pre){
-			String[] s1 = {"aaaaaaaaaaa","aaaaaaaaaaaaaa","aaaaaaaa","aa","a","aaaaaa","aaa"};
-			
-			//FullScreen
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			GraphicsDevice gd = ge.getDefaultScreenDevice();
-			gd.setFullScreenWindow(this);
-			//Container
-			cntnr = getContentPane();
-			cntnr.setLayout(new GridLayout(2, 1));
-			//Panel
-			p1 = new JPanel(new FlowLayout());
-			p2 = new JPanel(new FlowLayout());
-			p3 = new JPanel(new GridLayout(2, 3));
-			p4 = new JPanel(new GridLayout(2, 1));
-			//Button
-			topBtn = new JButton("Top");
-			serchBtn = new JButton("検索");
-			//Label
-			modeLbl = new JLabel("検索モード");
-			nameLbl = new JLabel("タイトル");
-			categoryLbl = new JLabel("カテゴリ");
-			stockLbl = new JLabel("在庫");
-			//TextField
-			serchText = new JTextField(20);
-			//List
-			nameList = new JList<String>(s1);
-			categoryList = new JList<String>(s1);
-			stockList = new JList<String>(s1);
-			
-			topBtn.addActionListener(this);
-			serchBtn.addActionListener(this);
-			
-			p1.add(topBtn);
-			p1.add(modeLbl);
-			p2.add(serchText);
-			p2.add(serchBtn);
-			p3.add(nameLbl);
-			p3.add(categoryLbl);
-			p3.add(stockLbl);
-			p3.add(nameList);
-			p3.add(categoryList);
-			p3.add(stockList);
-			p4.add(p1);
-			p4.add(p2);
-			cntnr.add(p4);
-			cntnr.add(p3);
-			setDefaultCloseOperation(EXIT_ON_CLOSE);
-			pre = false;
-			this.setVisible(false);
-		}
+		String[] s1 = {"                      ","","","","","","","",""};
+		//FullScreen
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
+		gd.setFullScreenWindow(this);
+		//Container
+		cntnr = getContentPane();
+		cntnr.setLayout(new GridLayout(2, 1));
+		//Panel
+		p1 = new JPanel(new FlowLayout());
+		p2 = new JPanel(new FlowLayout());
+		p3 = new JPanel(new GridLayout(2, 3));
+		p4 = new JPanel(new GridLayout(2, 1));
+		//Button
+		topBtn = new JButton("Top");
+		serchBtn = new JButton("検索");
+		//Label
+		modeLbl = new JLabel("検索モード");
+		nameLbl = new JLabel("タイトル");
+		categoryLbl = new JLabel("カテゴリ");
+		stockLbl = new JLabel("在庫");
+		//TextField
+		serchText = new JTextField(20);
+		//List
+		nameList = new JList<String>(s1);
+		categoryList = new JList<String>(s1);
+		stockList = new JList<String>(s1);
+		
+		topBtn.addActionListener(this);
+		serchBtn.addActionListener(this);
+		
+		p1.add(topBtn);
+		p1.add(modeLbl);
+		p2.add(serchText);
+		p2.add(serchBtn);
+		p3.add(nameLbl);
+		p3.add(categoryLbl);
+		p3.add(stockLbl);
+		p3.add(nameList);
+		p3.add(categoryList);
+		p3.add(stockList);
+		p4.add(p1);
+		p4.add(p2);
+		cntnr.add(p4);
+		cntnr.add(p3);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(false);
 	}
 	
 	public void actionPerformed(ActionEvent e){
@@ -78,7 +70,13 @@ public class SerchScreen extends JFrame implements ActionListener{
         if(e.getSource() == topBtn){
 			s.visibleControl(1);
 		}else if(e.getSource() == serchBtn){
-			
+		 	DBManager db= new DBManager();
+            List<String>  returnserchBookName = db.serchBookName(serchText.getText());
+			nameList.setListData(returnserchBookName.toArray());
+            List<String>  returnserchBookCategory = db.serchBookCategory(serchText.getText());
+			categoryList.setListData(returnserchBookCategory.toArray());
+            List<Integer>  returnserchBookStock = db.serchBookStock(serchText.getText());
+			stockList.setListData(returnserchBookStock.toArray());
         }
     }
 }
