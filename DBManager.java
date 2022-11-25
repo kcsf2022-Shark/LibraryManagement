bec6b4d543bcabcb4b3cfa049914beea2a8aba64
TaiseiKoga <73147431+KT-kcsf@users.noreply.github.com>
2022-11-25 09:39:45 +0900

    Add files via upload

--- DBManager.java
+++ DBManager.java
@@ -28,10 +28,12 @@ public class DBManager{
 		//貸出画面のuserConfirm押下時に呼び出される
 		try{
 			result = stmt.executeQuery("SELECT user_id FROM  user WHERE user_id = " + user + ";");
-			if(result.getString("user_id").equals("")){
-				return false;
-			}else{
-				return true;
+			if(result.next()){
+				if(result.getString("user_id").equals("")){
+					return false;
+				}else{
+					return true;
+				}
 			}
 		}catch(SQLException e){
 			e.printStackTrace();
@@ -43,10 +45,12 @@ public class DBManager{
 		//貸出画面のbookConfirm押下時に呼び出される
 		try{
 			result = stmt.executeQuery("SELECT book_id FROM book_manager WHERE book_id = " + book + ";");
-			if(result.getString("book_id").equals("")){
-				return false;
-			}else{
-				return true;
+			if(result.next()){
+				if(result.getString("book_id").equals("")){
+					return false;
+				}else{
+					return true;
+				}
 			}
 		}catch(SQLException e){
 			e.printStackTrace();
@@ -56,19 +60,17 @@ public class DBManager{
 	
 	public void lendBook(int user, int book){
 		//貸出画面のsubmit押下時に呼び出される
-		nowDate = new Date();//現在時刻を取得
-		calendar = Calendar.getInstance();
-		calendar.setTime(nowDate);//カレンダーにnowDateの時刻を入れる
-		calendar.add(Calendar.DAY_OF_MONTH, 7);//７日加算
-		returnSche = calendar.getTime();//加算後の時刻を入れる
+		int i = 0;
+		
 		try{
 			//borrow_user表に図書貸出情報を追加する
 			result = stmt.executeQuery("SELECT stock FROM book_manager WHERE book_id = " + book + ";");
-			int i = result.getInt("stock");
+			if(result.next()){
+				i = result.getInt("stock");
+			}
 			if(0 < i){
-				stmt.executeUpdate("UPDATE book_manager SET stock = " + (i - 1) + ";");
-				stmt.executeUpdate("INSERT INTO borrow_user VALUES(" +
-									user + "," + book + "," + nowDate + "," + returnSche + ");");
+				stmt.executeUpdate("UPDATE book_manager SET stock = " + (i - 1) + " WHERE book_id = " + book + ";");
+				stmt.executeUpdate("INSERT INTO borrow_user VALUES(" + user + "," + book + ");");
 			}else{
 				
 			}
@@ -81,10 +83,12 @@ public class DBManager{
 		//返却画面のbookConfirm押下時に呼び出される
 		try{
 			result = stmt.executeQuery("SELECT book_id FROM borrow_user WHERE user_id = " + user + " and book_id = " + book + ";");
-			if(result.getString("book_id").equals("")){
-				return false;
-			}else{
-				return true;
+			if(result.next()){
+				if(result.getString("book_id").equals("")){
+					return false;
+				}else{
+					return true;
+				}
 			}
 		}catch(SQLException e){
 			e.printStackTrace();
@@ -96,13 +100,15 @@ public class DBManager{
 		//返却画面のsubmit押下時に呼び出される
 		int cnt = 0;
 		try{
-			for(int i = 0; result.next() == true; i++){
-				stmt.executeUpdate("DELETE FROM borrow_user WHERE book_id = " + list.get(i) + ";");
+			for(int i : list){
+				stmt.executeUpdate("DELETE FROM borrow_user WHERE book_id = " + i + ";");
 			}
-			for(int i = 0; result.next() == true; i++){
-				result = stmt.executeQuery("SELECT stock FROM book_manager WHERE book_id = " + list.get(i) + ";");
-				cnt = result.getInt("stock") + 1;
-				stmt.executeUpdate("UPDATE book_manager SET stock = " + cnt + " WHERE book_id = " + list.get(i) + ";");
+			for(int i : list){
+				result = stmt.executeQuery("SELECT stock FROM book_manager WHERE book_id = " + i + ";");
+				if(result.next()){
+					cnt = result.getInt("stock") + 1;
+				}
+				stmt.executeUpdate("UPDATE book_manager SET stock = " + cnt + " WHERE book_id = " + i + ";");
 			}
 		}catch(SQLException e){
 			e.printStackTrace();
@@ -111,19 +117,12 @@ public class DBManager{
 	
 	public List<String> returnSche(int user){
 		//返却画面のuserConfirm押下時に呼び出される
-		Date returnDate;
-		nowDate = new Date();
-		List<String> nameList = new ArrayList<>();
+		List<String> nameList = new ArrayList<String>();
 		try{
+			
+			result = stmt.executeQuery(/*borrow_userに登録されたuserが借りている本の名前をselectする(user_idは引数で渡される。)*/);
 			while(result.next()){
-				result = stmt.executeQuery("SELECT book_manager.book_name borrow_user.scheduled_return_date " +
-											"FROM book_manager borrow_user WHERE book_manager.book_id = borrow_user.book_id;");
-				returnDate = result.getDate("sheduled_return_date");
-				if(returnDate.before(nowDate) == true){
-					nameList.add(result.getString("book_name"));
-				}else{
-					nameList.add(result.getString("book_name") + "[遅]");
-				}
+				nameList.add(result.getString("book_name"));
 			}
 		}catch(SQLException e){
 			e.printStackTrace();
@@ -135,8 +134,8 @@ public class DBManager{
 		//検索ボタンのserchBtn押下時に呼び出される
 		List<String> nameList = new ArrayList<>();
 		try{
+			result = stmt.executeQuery("SELECT book_name FROM book_manager WHERE book_name LIKE '%" + name + "%';");
 			while(result.next()){
-				result = stmt.executeQuery("SELECT book_name FROM book_manager WHERE book_name LIKE '%" + name + "%';");
 				nameList.add(result.getString("book_name"));
 			}
 		}catch(SQLException e){
@@ -149,8 +148,8 @@ public class DBManager{
 		//検索ボタンのserchBtn押下時に呼び出される
 		List<String> categoryList = new ArrayList<>();
 		try{
+			result = stmt.executeQuery("SELECT category FROM book_manager WHERE book_name LIKE '%" + name + "%';");
 			while(result.next()){
-				result = stmt.executeQuery("SELECT category FROM book_manager WHERE book_name LIKE '%" + name + "%';");
 				categoryList.add(result.getString("category"));
 			}
 		}catch(SQLException e){
@@ -163,8 +162,8 @@ public class DBManager{
 		//検索ボタンのserchBtn押下時に呼び出される
 		List<Integer> stockList = new ArrayList<>();
 		try{
+			result = stmt.executeQuery("SELECT stock FROM book_manager WHERE book_name LIKE '%" + name + "%';");
 			while(result.next()){
-				result = stmt.executeQuery("SELECT stock FROM book_manager WHERE book_name LIKE '%" + name + "%';");
 				stockList.add(result.getInt("stock"));
 			}
 		}catch(SQLException e){
